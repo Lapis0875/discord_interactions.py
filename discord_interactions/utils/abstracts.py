36d810ea5416255961b4c257fb8dc71cee344c43
Minjun Kim (Lapis0875) <lapis0875@kakao.com>
2021-01-18 22:59:59 +0900

    feat: Update utils/abstracts.py
    Add docstrings

--- discord_interactions/utils/abstracts.py
+++ discord_interactions/utils/abstracts.py
@@ -4,20 +4,26 @@ from .type_hints import JSON
 
 
 class JsonObject(metaclass=ABCMeta):
+    """Abstract Base Class for Json-convertible python objects"""
 
     @abstractmethod
-    def toJson(cls) -> JSON:
-        """
+    def toJson(self) -> JSON:
+        """Parse object into json
 
+        Returns:
+            Parse self into dictionary object in json format.
         """
-        ...
+        pass
 
     @classmethod
     @abstractmethod
     def fromJson(cls, data: JSON) -> JsonObject:
         """Parse json data into JsonObject
 
-        :param data:
-        :return:
+        Args:
+            data (JSON): dictionary object containing json data.
+
+        Returns:
+            Parsed json object.
         """
-        ...
+        pass
